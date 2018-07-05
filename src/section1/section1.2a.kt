60f03b89378cd787309cb52c75ed30cfc42b4600
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-05 10:17:14 +0900

    Simplified the code

--- src/section1/section1.2a.kt
+++ src/section1/section1.2a.kt
@@ -1,14 +1,9 @@
 package section1
 
 fun main(args: Array<String>) {
-    println("Enter age: ")
-    val age = try {
-        readLine()?.toInt()
-    } catch (e: NumberFormatException) {
-        0
-    }
-    if (age != null)
-        println("Can drive in ${checkCountries(age)}")
+
+    val age = 16
+    println("Can drive in ${checkCountries(age)}")
 }
 
 fun checkCountries(age: Int): String {
