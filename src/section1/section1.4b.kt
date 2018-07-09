387988baff578be8a8611715c5ad5453758ff5ed
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-09 14:02:21 +0900

    update

--- src/section1/section1.4b.kt
+++ src/section1/section1.4b.kt
@@ -3,6 +3,9 @@ package section1
 fun main(args: Array<String>) {
     val tel = StringBuilder("819012345678")
     println("Number = $tel")
-    println("Country code = ${tel.substring(0, 2)}")
+    println("Country code = ${tel.countryCode}")
 }
+
+val StringBuilder.countryCode: String
+    get() = this.substring(0, 2)
 
