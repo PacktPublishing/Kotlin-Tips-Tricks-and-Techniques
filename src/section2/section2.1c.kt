99d725030e65e4df27bbf2756cb3e80197f8c70d
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-13 12:27:43 +0900

    update

--- src/section2/section2.1c.kt
+++ src/section2/section2.1c.kt
@@ -3,14 +3,12 @@ package section2
 open class Try(val points: Int)
 
 fun main(args: Array<String>) {
-//    val total: Any = 15
-    val total = object : Try(15){}
-//    val rugby = total as? Try
-//    println(rugby?.points)
+
+    val total: Any = 15
     println(score(total))
 }
 
 fun score(total: Any): Int {
-    return (total as? Try)?.points ?: 0
+    return (total as Try).points
 }
 
