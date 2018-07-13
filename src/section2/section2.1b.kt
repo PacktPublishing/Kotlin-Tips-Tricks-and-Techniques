aa204e4b0e4dcc7f98505db13fb8c346cb4a34ef
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-13 11:44:01 +0900

    update

--- src/section2/section2.1b.kt
+++ src/section2/section2.1b.kt
@@ -4,9 +4,10 @@ interface Score
 open class Run(val runs: Int) : Score
 
 fun main(args: Array<String>) {
+
     val total: Any = 123
-//    val total = object : Run(123){}
-    val cricket = total as? Run
-    println(cricket?.runs)
+    val cricket = total as Run
+
+    println(cricket.runs)
 }
 
