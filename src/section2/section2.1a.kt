eb3b40a3262045d47d22927716b8ce556c90b604
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-13 11:29:15 +0900

    update

--- src/section2/section2.1a.kt
+++ src/section2/section2.1a.kt
@@ -2,8 +2,6 @@ package section2
 
 fun main(args: Array<String>) {
 
-    val number = "+447198765432"
-
     var phoneNumber: String? = null
 
     println(if (phoneNumber != null) phoneNumber else "+447911123456")
