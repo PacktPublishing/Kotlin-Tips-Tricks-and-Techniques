e112b83ead4434c00fd262a8123d5a40facda7c3
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-19 10:04:14 +0900

    restructured code

--- src/section1/section1.2a.kt
+++ src/section1/section1.2a.kt
@@ -7,15 +7,10 @@ fun main(args: Array<String>) {
 }
 
 fun checkCountries(age: Int): String {
-    return if (age < 14)
-        "no country"
-    else if (age < 15)
-        "Guyana"
-    else if (age < 16)
-        "Guyana and Canada"
-    else if (age < 17)
-        "Guyana, Canada and France"
-    else if (age < 18)
-        "Guyana, Canada, France and Alberta"
+    return if (age < 14) "no country"
+    else if (age < 15) "Guyana"
+    else if (age < 16) "Guyana and Canada"
+    else if (age < 17) "Guyana, Canada and France"
+    else if (age < 18) "Guyana, Canada, France and Alberta"
     else "any country"
 }
