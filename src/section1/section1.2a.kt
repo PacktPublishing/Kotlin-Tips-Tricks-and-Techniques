e2f51d31f509c429820c4c62e56e2cb16be8f8b5
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-19 09:32:20 +0900

    converted to block body

--- src/section1/section1.2a.kt
+++ src/section1/section1.2a.kt
@@ -6,15 +6,16 @@ fun main(args: Array<String>) {
     println("Can drive in ${checkCountries(age)}")
 }
 
-fun checkCountries(age: Int) =
-        if (age < 14)
-            "no country"
-        else if (age < 15)
-            "Guyana"
-        else if (age < 16)
-            "Guyana and Canada"
-        else if (age < 17)
-            "Guyana, Canada and France"
-        else if (age < 18)
-            "Guyana, Canada, France and Alberta"
-        else "any country"
+fun checkCountries(age: Int): String {
+    return if (age < 14)
+        "no country"
+    else if (age < 15)
+        "Guyana"
+    else if (age < 16)
+        "Guyana and Canada"
+    else if (age < 17)
+        "Guyana, Canada and France"
+    else if (age < 18)
+        "Guyana, Canada, France and Alberta"
+    else "any country"
+}
