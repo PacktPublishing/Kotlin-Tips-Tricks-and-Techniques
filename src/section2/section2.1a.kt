fb73efb90deae9a401655c8402f578b8f5bb9731
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-21 11:56:57 +0900

    update

--- src/section2/section2.1a.kt
+++ src/section2/section2.1a.kt
@@ -4,7 +4,14 @@ fun main(args: Array<String>) {
 
     var phoneNumber: String? = null
 
-    println(null ?: "+447911123456")
+    println(checkNumber(phoneNumber))
 
+}
+
+fun checkNumber(data: String?): String {
+    return if (data != null)
+        data
+    else
+        "+447911123456"
 }
 
