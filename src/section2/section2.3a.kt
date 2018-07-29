7cf6b5490878ff10554368387408fd7a42454463
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-29 15:46:36 +0900

    update

--- src/section2/section2.3a.kt
+++ src/section2/section2.3a.kt
@@ -1,7 +1,9 @@
 package section2
 
 fun main(args: Array<String>) {
-    val scores = listOf("45", null, 120, null)
-    for (score in scores)
-        println(score)
+
+    var scores: Array<String?>? = null
+    scores = arrayOf("45", null, "120", null)
+
+
 }
