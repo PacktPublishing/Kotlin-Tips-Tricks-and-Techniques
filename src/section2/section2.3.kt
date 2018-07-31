7ed550b08784b3112090977cdea1ca5ca18028ae
Nigel Henshaw <nigel@mobapptuts.com>
2018-07-31 12:57:20 +0900

    update

---
+++ src/section2/section2.3.kt
@@ -0,0 +1,10 @@
+package section2
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    var scores: List<String?>? = null
+    scores = listOf("45", null, "120", null)
+
+}
--- src/section2/section2.3a.kt
+++ src/section2/section2.3a.kt
@@ -1,10 +1,6 @@
 package section2
 
-import java.util.*
-
 fun main(args: Array<String>) {
-
-    var scores: List<String?>? = null
-    scores = listOf("45", null, "120", null)
+    val scores = arrayListOf("45", null, "120", null)
 
 }
