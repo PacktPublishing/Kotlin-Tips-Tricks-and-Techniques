7264a7ab31923105a3a33a462cac6a4b40763157
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-03 09:08:00 +0900

    update

--- src/section2/section2.4a.kt
+++ src/section2/section2.4a.kt
@@ -1,2 +1,25 @@
 package section2
 
+class SharedObject(var sharedData: String = "")
+
+class BasicThreadOne(var shared: SharedObject): Thread() {
+    override fun run() {
+        super.run()
+
+        shared.sharedData = Thread.currentThread().toString()
+        Thread.sleep(2000)
+    }
+}
+
+class BasicThreadTwo(var shared: SharedObject): Thread() {
+    override fun run() {
+        super.run()
+
+        shared.sharedData = Thread.currentThread().toString()
+    }
+}
+
+fun main(args: Array<String>) {
+
+}
+
