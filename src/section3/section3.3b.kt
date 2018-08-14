62b9240cc57822c322cccae82331519df8a2f1ae
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-14 09:43:35 +0900

    update

--- src/section3/section3.3b.kt
+++ src/section3/section3.3b.kt
@@ -1,2 +1,9 @@
 package section3
 
+class ExamResults {
+    var results: ArrayList<Double>? = null
+}
+
+fun main(args: Array<String>) {
+    val scores = arrayListOf(25.0, 0.0, 77.7, 0.0, 50.0)
+}
