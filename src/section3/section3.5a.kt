c98a6f5c4210aff2dcb886354d96b618123268da
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-15 13:17:31 +0900

    new file

--- src/section3/section3.5a.kt
+++ src/section3/section3.5a.kt
@@ -1,2 +1,13 @@
 package section3
 
+class CompanionClass {
+    private val list = arrayListOf(1.2, 3.3, 4.5)
+
+    fun displayList() = println(list)
+}
+
+fun main(args: Array<String>) {
+    val companionClass = CompanionClass()
+    companionClass.displayList()
+
+}
