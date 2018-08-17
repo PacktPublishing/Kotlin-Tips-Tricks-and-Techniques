a8a39121e3daaf9bf54bc598715432baddb31290
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-17 10:07:54 +0900

    update

--- src/section4/section4.1a.kt
+++ src/section4/section4.1a.kt
@@ -1,2 +1,10 @@
 package section4
 
+fun main(args: Array<String>) {
+    val total: Int = sum(2, 3)
+    println(total)
+}
+
+fun sum(value1: Int, value2: Int): Int {
+    return value1 + value2
+}
