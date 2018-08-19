32d1b1d7e9f1aacc32f3480338b14ff68771e5ae
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-19 14:19:29 +0900

    update starting code

--- src/section4/section4.2a.kt
+++ src/section4/section4.2a.kt
@@ -2,14 +2,8 @@ package section4
 
 fun main(args: Array<String>) {
 
-    displayTotal(total(2, 3))
-
 }
 
-fun total(value1: Int, value2: Int): Int {
-    return value1 + value2
-}
+fun add(value1: Int, value2: Int) = value1 + value2
+fun multiply(value1: Int, value2: Int) = value1 * value2
 
-fun displayTotal(total:  Int) {
-    println(total)
-}
