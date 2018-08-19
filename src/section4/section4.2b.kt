cb6c872561ec890eafbe0ba858aa3108be201de1
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-19 14:37:22 +0900

    starting code

--- src/section4/section4.2b.kt
+++ src/section4/section4.2b.kt
@@ -1,2 +1,24 @@
 package section4
 
+fun main(args: Array<String>) {
+
+}
+
+fun thread(
+        start: Boolean = true,
+        name: String? = null,
+        priority: Int = -1
+): Thread {
+    val thread = object : Thread() {
+        public override fun run() {
+            println(Thread.currentThread())
+        }
+    }
+    if (priority > 0)
+        thread.priority = priority
+    if (name != null)
+        thread.name = name
+    if (start)
+        thread.start()
+    return thread
+}
