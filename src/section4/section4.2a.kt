97addf94bd133ccd60e6b9ef56038243bbb4103f
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-19 13:32:35 +0900

    Initial code

--- src/section4/section4.2a.kt
+++ src/section4/section4.2a.kt
@@ -1,2 +1,15 @@
 package section4
 
+fun main(args: Array<String>) {
+
+    displayTotal(total(2, 3))
+
+}
+
+fun total(value1: Int, value2: Int): Int {
+    return value1 + value2
+}
+
+fun displayTotal(total:  Int) {
+    println(total)
+}
