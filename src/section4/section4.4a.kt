47a9e36ccba8f30a5835a4c544e2064118bae1c8
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-23 15:13:41 +0900

    update

--- src/section4/section4.4a.kt
+++ src/section4/section4.4a.kt
@@ -1,2 +1,40 @@
 package section4
 
+class MediaMetaData(
+        val id: Int = 0,
+        val title: String,
+        val artist: String,
+        val album: String
+)
+
+fun main(args: Array<String>) {
+
+    val musicSorce = listOf(
+            MediaMetaData(
+                    100,
+                    "Come Fly with Me",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaData(
+                    101,
+                    "Brazil",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaData(
+                    102,
+                    "Blue Hawaii",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaData(
+                    103,
+                    "Suspicion",
+                    "Elvis Presley",
+                    "Pot Luck"
+            )
+    )
+
+}
+
