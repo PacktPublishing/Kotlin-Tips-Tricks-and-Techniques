aef04ca798442d061c4931b190361ad9bd415958
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-24 14:44:29 +0900

    starting code

--- src/section4/section4.5a.kt
+++ src/section4/section4.5a.kt
@@ -1,2 +1,15 @@
 package section4
 
+private class MusicMetaData(
+        val id: Int = 0,
+        val title: String,
+        val artist: String
+)
+
+fun main(args: Array<String>) {
+    val source = MusicMetaData(
+            100,
+            "Come Fly with Me",
+            "Frank Sinatra"
+    )
+}
