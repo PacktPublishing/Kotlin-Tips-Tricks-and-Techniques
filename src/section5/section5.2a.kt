b7283273e19c68259ac1fe47e71e6f94564f94a4
Nigel Henshaw <nigel@mobapptuts.com>
2018-08-30 21:05:53 +0900

    starting code

--- src/section5/section5.2a.kt
+++ src/section5/section5.2a.kt
@@ -1,22 +1,34 @@
 package section5
 
-class ExamResults {
-    var intResults : ArrayList<Double>? = null
-    set(value) {
-        value!!.removeAll(arrayListOf(0.0))
-        field = value
-    }
-
-    val average: Double
-    get() {
-        return intResults!!.average()
+class BackGroundThread : Thread() {
+    override fun run() {
+        println(Thread.currentThread())
     }
 }
 
+var backgroundThread: BackGroundThread? = null
+
 fun main(args: Array<String>) {
-//    val intScores = arrayListOf(25.0, 0.0, 77.7, 0.0, 50.0)
-    val examResults = ExamResults()
-//    examResults.intResults = intScores
-    println(examResults.intResults)
-    println(examResults.average)
+
+    createBackgroundThread()
+    startBackgroundThread()
+    stopBackgroundThread()
+
+}
+
+fun createBackgroundThread() {
+    backgroundThread = BackGroundThread()
+}
+
+fun startBackgroundThread() {
+        backgroundThread!!.start()
+    }
+
+fun stopBackgroundThread() {
+    try {
+        backgroundThread!!.join()
+        backgroundThread == null
+    } catch (e: InterruptedException) {
+        println(e.printStackTrace())
+    }
 }
