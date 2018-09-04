9543cb8e3789f61efa639976c58ce9ae020889e0
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-04 11:30:09 +0900

    update

--- src/section5/section5.3b.kt
+++ src/section5/section5.3b.kt
@@ -4,6 +4,6 @@ fun main(args: Array<String>) {
 
 }
 
-inline fun functionArgumentLambda(task: (Int, Int) -> Int) {
+fun functionArgumentLambda(task: (Int, Int) -> Int) {
     println(task(2, 3))
 }
