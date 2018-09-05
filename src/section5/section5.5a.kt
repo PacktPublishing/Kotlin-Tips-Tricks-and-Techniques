da400ee12ef0a7b1b5aba2d9ab1779b32ceb380e
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-05 15:35:39 +0900

    starting code

--- src/section5/section5.5a.kt
+++ src/section5/section5.5a.kt
@@ -1,2 +1,56 @@
 package section5
 
+class MediaMetaSequence(
+        val id: Int = 0,
+        val title: String,
+        val artist: String,
+        val album: String
+)
+
+fun main(args: Array<String>) {
+
+    val musicSorce = listOf(
+            MediaMetaSequence(
+                    100,
+                    "Come Fly with Me",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaSequence(
+                    101,
+                    "Brazil",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaSequence(
+                    102,
+                    "Blue Hawaii",
+                    "Frank Sinatra",
+                    "Come Fly with me"
+            ),
+            MediaMetaSequence(
+                    103,
+                    "Suspicion",
+                    "Elvis Presley",
+                    "Pot Luck"
+            )
+    )
+
+    val playlist = buildPlayList(musicSorce, "Come Fly with me")
+    val albumTitles = displayAlbumTitles(playlist, "Come Fly with me")
+    println("title: ${albumTitles}")
+}
+
+fun displayAlbumTitles(source: List<MediaMetaSequence>, album: String): List<String> {
+    return source.filter {
+        it.album == album
+    }.map {
+        it.title
+    }
+}
+
+fun buildPlayList(source: List<MediaMetaSequence>, album: String): List<MediaMetaSequence> {
+    return source.filter {
+        it.album == album
+    }
+}
