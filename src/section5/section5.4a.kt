2afdffbb5814735bf35c517eeaecd37ff663b35a
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-06 11:14:54 +0900

    updating starting code

--- src/section5/section5.4a.kt
+++ src/section5/section5.4a.kt
@@ -3,12 +3,11 @@ package section5
 class MetaData {
 
     companion object {
-        fun newInstance() = MetaData()
+
     }
 
 }
 
 fun main(args: Array<String>) {
 
-    val meta = MetaData.newInstance()
 }
