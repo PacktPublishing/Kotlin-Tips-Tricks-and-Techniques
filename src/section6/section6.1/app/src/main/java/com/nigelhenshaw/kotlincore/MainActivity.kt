5978c5aa962c81b79e81f797151023a471474a5e
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-09 13:48:16 +0900

    New project code

---
+++ src/section6/section6.1/app/src/main/java/com/nigelhenshaw/kotlincore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nigelhenshaw.kotlincore
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
