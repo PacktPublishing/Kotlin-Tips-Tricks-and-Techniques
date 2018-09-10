c891710238c02871ca2b432a287fd00d2ad44642
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-10 14:06:17 +0900

    Starting android project code for SQLite KTX exercise

---
+++ src/section6/section6.2/app/src/main/java/com/nigelhenshaw/sqlitektx/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nigelhenshaw.sqlitektx
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
