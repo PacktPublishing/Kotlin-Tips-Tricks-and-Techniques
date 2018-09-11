512c3d82165d8245ca9b458dd3fbb7da8dff730d
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-11 13:21:38 +0900

    Added two fragments

--- src/section6/section6.3/app/src/main/java/com/nigelhenshaw/ktxfragment/MainActivity.kt
+++ src/section6/section6.3/app/src/main/java/com/nigelhenshaw/ktxfragment/MainActivity.kt
@@ -2,11 +2,20 @@ package com.nigelhenshaw.ktxfragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentOneButton.setOnClickListener {
+
+        }
+
+        fragmentTwoButton.setOnClickListener {
+
+        }
     }
 }
