6a6fd3ad5d24471a1f7d1cef9ab662d5ae92ec04
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-12 10:51:18 +0900

    starting code

--- src/section6/section6.3/app/src/main/java/com/nigelhenshaw/ktxfragment/FirstFragment.kt
+++ src/section6/section6.3/app/src/main/java/com/nigelhenshaw/ktxfragment/FirstFragment.kt
@@ -2,10 +2,10 @@ package com.nigelhenshaw.ktxfragment
 
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 class FirstFragment : Fragment() {
