4d8d091f1092647fa539d7ae6a0590361113e4d0
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 15:50:26 +0900

    added comments

--- src/section3/section3.3b.kt
+++ src/section3/section3.3b.kt
@@ -1,9 +1,17 @@
 package section3
 
+/**
+ * This class will be used to apply a custom setter to
+ * results property.
+ * Which will be used to remove any 0.0 values from the property
+ */
 class ExamResults {
     var results: ArrayList<Double>? = null
 }
 
 fun main(args: Array<String>) {
+    // Create an instance of ExamResults
+    // Populate its property with scores, then display the
+    // contents of the instance
     val scores = arrayListOf(25.0, 0.0, 77.7, 0.0, 50.0)
 }
