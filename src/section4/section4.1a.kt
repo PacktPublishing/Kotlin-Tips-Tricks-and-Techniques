a75508282fadf896440c22e1a3b0892aa85f3d99
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 16:15:12 +0900

    Added comments

--- src/section4/section4.1a.kt
+++ src/section4/section4.1a.kt
@@ -1,10 +1,17 @@
 package section4
 
 fun main(args: Array<String>) {
+    // A lambda expression will be added
+    // to replace the Sum() function
     val total = sum(2, 3)
     println(total)
 }
 
+/**
+ * This function is used at the start of the
+ * exercise for demonstration purposes.
+ * It will then be replaced with a Lamda expression.
+ */
 fun sum(value1: Int, value2: Int): Int {
     return value1 + value2
 }
