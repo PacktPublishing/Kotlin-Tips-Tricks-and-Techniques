1b28cc7fb9c37c0f849f5182596baf2b1a7039f2
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 16:20:20 +0900

    Added comments

--- src/section4/section4.1b.kt
+++ src/section4/section4.1b.kt
@@ -1,6 +1,16 @@
 package section4
 
+/**
+ * Create a function called groupByLength
+ * to group the list collection by size
+ */
+
+
+
 fun main(args: Array<String>) {
+    // Call the newly created groupByLength function
+    // and pass in the words value.
+    //Display the result returned from the groupByLength method.
     val words = listOf("a", "abc", "ab", "def", "abcd")
 
 }
