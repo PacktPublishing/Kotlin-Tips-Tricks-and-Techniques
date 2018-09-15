a46d420fe8bf9a1eb463067ddf55cb91305f1509
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 16:32:12 +0900

    Added comments

--- src/section4/section4.2a.kt
+++ src/section4/section4.2a.kt
@@ -1,9 +1,22 @@
 package section4
 
+/**
+ * Add a function called functionArgument, which will support a function being passed as it’s parameter.
+ * To pass a function as an argument we need to provided its function type.
+ */
+
+
 fun main(args: Array<String>) {
+    // Call the newly created functionArgument() function
+    // and use a member reference to pass the multiply function as a argument
 
 }
 
+/**
+ * The functions be will be used initially as member references to be passed
+ * as arguments to functionArgument()
+ * The will then be replaced with Lambdas which have the same implementation
+ */
 fun add(value1: Int, value2: Int) = value1 + value2
 fun multiply(value1: Int, value2: Int) = value1 * value2
 
