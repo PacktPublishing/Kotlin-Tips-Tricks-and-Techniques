10754aeaf81a84950d4cd7a708927e9f0916a1a9
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 11:04:13 +0900

    Starting code for WorkManager exercise

---
+++ src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/ColorWorkers.kt
@@ -0,0 +1,9 @@
+package com.nigelhenshaw.ktxwork
+
+import com.nigelhenshaw.ktxwork.MainActivity.Companion.KEY_GET_COLOR
+import com.nigelhenshaw.ktxwork.MainActivity.Companion.KEY_SET_COLOR
+
+private val colors by lazy {
+    arrayOfNulls<String>(4)
+}
+
--- src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
+++ src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.nigelhenshaw.ktxwork
 
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val KEY_SET_COLOR: String = "key_set_color"
+        const val KEY_GET_COLOR: String = "key_get_color"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun loadList(colors: Array<String?>?) {
+        colors?.let {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, colors)
+            colorListView.adapter = adapter
+        }
     }
 }
