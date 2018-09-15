642d72aa923bdb493e1fb22dbd644afd6beb964d
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 11:15:30 +0900

    minor update

--- src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
+++ src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.nigelhenshaw.ktxwork
 
-import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
