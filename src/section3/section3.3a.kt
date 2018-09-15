d271080f905c8ccf94a62f3ac304250f5da39014
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 15:40:25 +0900

    added comments

--- src/section3/section3.3a.kt
+++ src/section3/section3.3a.kt
@@ -1,11 +1,18 @@
 package section3
 
+/**
+ * UseAccount class will be used to demonstrate
+ * how to create a custom accessor for the name property
+ */
 class UserAccount(_id: Int, _name: String) {
     val id = _id
     val name = _name
 }
 
+
 fun main(args: Array<String>) {
+    // Create an instance of UserAccount
+    // Then display its contents
 
 }
 
