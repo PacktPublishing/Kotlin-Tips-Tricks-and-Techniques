f9a6e38f15c4626403cbe0e42e3a6232ef5bb841
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 16:45:32 +0900

    Added comments

--- src/section4/section4.3a.kt
+++ src/section4/section4.3a.kt
@@ -1,10 +1,30 @@
 package section4
 
+/**
+ * This class will be used to contain three properties
+ * 1/ mutable list
+ * 2/ readOnly mutable list
+ * 3/ immutable list
+ * Each of the properties for will tested for thread safety
+ */
 class StateObjects {
 
 }
 
+/**
+ * Create new function modifyMutableList() to modify mutable list
+ */
+
+/**
+ * Create new function modifyReadOnlyList() to modify read-only mutable list
+ */
+
+/**
+ * Create new function  immutableList() to attempt to modify immutable list
+ */
+
 fun main(args: Array<String>) {
+    // Call the three newly created functions to verify which ones are thread safe.
     val stateObjects = StateObjects()
 }
 
