a55a6c3e926883f4e97ea128b310d08e40f11198
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-15 15:58:19 +0900

    Added comments

--- src/section3/section3.4a.kt
+++ src/section3/section3.4a.kt
@@ -1,6 +1,15 @@
 package section3
 
-fun main(args: Array<String>) {
+/**
+ * Create a function called thread(), which returns a thread
+ * An anonymous object will be used to create a single instance
+ * of the Thread class
+ * Display the current thread information inside the object's
+ * run method.
+ */
 
+
+fun main(args: Array<String>) {
+    // Call the thread function
 }
 
