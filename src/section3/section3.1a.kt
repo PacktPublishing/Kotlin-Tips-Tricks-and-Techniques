09f487d135d2e149782f843462ba2b94f3bf1c76
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 20:57:55 +0900

    Added comments

--- src/section3/section3.1a.kt
+++ src/section3/section3.1a.kt
@@ -1,10 +1,18 @@
 package section3
 
 /**
- * Create a class with keyword constructor and initializer block
+ * Create a class StandardThreadThree with keyword constructor and initializer block
+ * The initializer block will then be replaced with a primary constructer
+ * After that the class will then extend the Thread class to demonstrate initializing
+ * the super class constructor.
  */
 
 
+/**
+ * This exercise will demonstrate the usage of the class primary constructor.
+ * The newly created Class will then extend the Thread class to demonstrate initializing
+ * super class constructors.
+ */
 fun main(args: Array<String>) {
     //Create StandardThreadThree instance and display timeout value
 
