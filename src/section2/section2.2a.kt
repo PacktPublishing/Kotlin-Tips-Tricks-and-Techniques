bf6e17ffa5554e068f589d3a4e65108eacc3bacd
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 14:54:49 +0900

    Added comments

--- src/section2/section2.2a.kt
+++ src/section2/section2.2a.kt
@@ -1,10 +1,21 @@
 package section2
 
+/**
+ * This exercise introduces the Let extenstion function to
+ * do null safe calls with the value tries
+ *
+ */
 fun main(args: Array<String>) {
 
     var tries: Int? = null
+    // Initialize tries with a value and then use the Let function
+    // to make null safe call when passing tries to the function convertToPoints
+
 }
 
+/**
+ * Use to test the null safety of the parameter tries
+ */
 fun convertToPoints(tries: Int) {
     println(tries * 5)
 }
