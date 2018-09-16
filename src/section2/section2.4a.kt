6e075e75f5c59a486cb33d05d112f7bf00cae0e7
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 20:34:45 +0900

    Added comments

--- src/section2/section2.4a.kt
+++ src/section2/section2.4a.kt
@@ -1,7 +1,15 @@
 package section2
 
+/**
+ * Object to be shared between the two threads
+ */
 class SharedObject(var sharedData: String = "")
 
+/**
+ * First thread to use the shared data.
+ * A synchronized lock will first be implemented. Then the ReentrantLock
+ * will replace the synchronized lock.
+ */
 class BasicThreadOne(var shared: SharedObject): Thread() {
     override fun run() {
         super.run()
@@ -11,6 +19,11 @@ class BasicThreadOne(var shared: SharedObject): Thread() {
     }
 }
 
+/**
+ * Second thread to use the shared data.
+ * A synchronized lock will first be implemented. Then the ReentrantLock
+ * will replace the synchronized lock.
+ */
 class BasicThreadTwo(var shared: SharedObject): Thread() {
     override fun run() {
         super.run()
@@ -19,6 +32,11 @@ class BasicThreadTwo(var shared: SharedObject): Thread() {
     }
 }
 
+/**
+ * This exercise will demonstrate thread safety by using a shared object
+ * A SharedObject instance will be passed to each of the BasicThread class instances.
+ * A timer will be used to test for thread contention
+ */
 fun main(args: Array<String>) {
 
 }
