355dbaa1e7b046ff8a0a79c669649a11515b45a1
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 15:06:06 +0900

    Added comments

--- src/section2/section2.3a.kt
+++ src/section2/section2.3a.kt
@@ -1,5 +1,11 @@
 package section2
 
+/**
+ * This exercise will involve the creation of a new value called scoresNotNull
+ * which will be made up of scores without the null elements
+ * First create scoresNotNull by stripping out the nulls in code.
+ * Then replace that implementation with scoresNotNull
+ */
 fun main(args: Array<String>) {
     val scores = arrayListOf("45", null, "120", null)
 
