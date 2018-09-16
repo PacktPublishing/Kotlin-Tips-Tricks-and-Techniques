46843a0795a3125c53490a9e02967095c1d8b31d
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 10:04:02 +0900

    Added comments

--- src/section4/section4.5a.kt
+++ src/section4/section4.5a.kt
@@ -1,12 +1,22 @@
 package section4
 
+/**
+ * Used to contain song meta data
+ * The universal methods will be added and demonstrated
+ * Then the universal methods will be replaced with the data class
+ */
 private class MusicMetaData(
         val id: Int = 0,
         val title: String,
         val artist: String
 )
 
+/**
+ * The source value will be used for checking the toString(),
+ * equals() and hashCode() methods in the MusicMetaData class
+ */
 fun main(args: Array<String>) {
+    // Create a single instance of MusicMetaData
     val source = MusicMetaData(
             100,
             "Come Fly with Me",
