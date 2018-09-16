b20e5d218477edb33259a95ac51dc473700acf7c
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 21:18:04 +0900

    Updated comments

--- src/section3/section3.3b.kt
+++ src/section3/section3.3b.kt
@@ -3,12 +3,18 @@ package section3
 /**
  * This class will be used to apply a custom setter to
  * results property.
- * Which will be used to remove any 0.0 values from the property
+ * Which will be used to remove any 0.0 values from the property.
+ * And then add a backing field to the setter.
  */
 class ExamResults {
     var results: ArrayList<Double>? = null
 }
 
+/**
+ * This exercise will demonstrate how to add a setter custom
+ * accessor to a property. And what will happen if a backing
+ * field is not used
+ */
 fun main(args: Array<String>) {
     // Create an instance of ExamResults
     // Populate its property with scores, then display the
