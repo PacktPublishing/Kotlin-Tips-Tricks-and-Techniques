b6b8db9e90f2e41552a39dc6f634c40a99746b1a
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 21:16:24 +0900

    Updated comments

--- src/section3/section3.3a.kt
+++ src/section3/section3.3a.kt
@@ -3,13 +3,18 @@ package section3
 /**
  * UseAccount class will be used to demonstrate
  * how to create a custom accessor for the name property
+ * that will capitalize the name property.
  */
 class UserAccount(_id: Int, _name: String) {
     val id = _id
     val name = _name
 }
 
-
+/**
+ * This exercise will demonstrate how to add a getter custom
+ * accessor to a property. And what will happen if a backing
+ * field is not used
+ */
 fun main(args: Array<String>) {
     // Create an instance of UserAccount
     // Then display its contents
