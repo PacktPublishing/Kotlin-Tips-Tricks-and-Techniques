95bda5e2511765a47077312602b41663b817eb15
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 11:23:27 +0900

    Added comments

--- src/section2/section2.1a.kt
+++ src/section2/section2.1a.kt
@@ -1,13 +1,18 @@
 package section2
 
+/**
+ * Main function will be used to pass a null argument
+ * to the checkNumber() function to demonstrate null type checking
+ */
 fun main(args: Array<String>) {
 
     var phoneNumber: String? = null
-
     println(checkNumber(phoneNumber))
-
 }
 
+/**
+ * Replace the if expression with the Elvis operator
+ */
 fun checkNumber(data: String?): String {
     return if (data != null)
         data
--- src/section2/section2.1b.kt
+++ src/section2/section2.1b.kt
@@ -1,10 +1,20 @@
 package section2
 
+/**
+ * The interface Score provides a common type to the class Run
+ */
 interface Score
+
+/**
+ * Class run will be used to type check against the value total
+ */
 open class Run(val runs: Int) : Score
 
 fun main(args: Array<String>) {
 
     val total: Any = 123
+    // Typecheck total against the Class Run. Then return the runs property and
+    // assign it to a new variable called runs
+    // Then add an Elvis operator to return zero if the type cast is unsuccessful
 }
 
