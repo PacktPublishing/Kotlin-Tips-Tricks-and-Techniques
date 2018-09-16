109bc45d721ababff3460ba8a7d9b23a31cb573c
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 20:47:36 +0900

    Added comments

--- src/section2/section2.5a.kt
+++ src/section2/section2.5a.kt
@@ -2,12 +2,35 @@ package section2
 
 import java.util.*
 
-
+/**
+ * Shared value is initially implemented as a class.
+ * Later on in the exercise it will be converted to an object
+ * for converting it to a singleton.
+ */
 class SharedValue {
     val list = ArrayList<String>()
 }
 
+/**
+ * Implement a thread class that adds the current thread information
+ * to the SharedValue list
+ */
 
+
+/**
+ * Implement a second thread class that adds the current thread information
+ * to the SharedValue list
+ */
+
+
+/**
+ * This exercise will compare objects against singletons.
+ * Create instances of each newly created thread class. And
+ * start both threads.
+ * Once the issues have been identified with using SharedValue instances, convert
+ * SharedValue to an object which will convert it to a singleton. Then check the issues
+ * have been resolved.
+ */
 fun main(args: Array<String>) {
 
 
