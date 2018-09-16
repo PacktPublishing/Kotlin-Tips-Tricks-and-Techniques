abbf1e5adecc284b490ad135daac6450975120a1
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-16 20:19:22 +0900

    Added comments

--- src/section2/section2.3b.kt
+++ src/section2/section2.3b.kt
@@ -1,5 +1,13 @@
 package section2
 
+/**
+ * This exercise will use the listOfNotNull() function
+ * to create a collection of nulls.
+ * Which will then get assigned to the value scoresNotNull.
+ * Note that the spread operator will need to be applied to
+ * the argument for unpacking the values.
+ * Then display the scoresNotNull value.
+ */
 fun main(args: Array<String>) {
 
     val scores = arrayOf("45", null, "120", null)
