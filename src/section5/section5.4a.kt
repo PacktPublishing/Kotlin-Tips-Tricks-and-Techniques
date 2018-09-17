693277a193a5a72d09555aa909a11fc7f73d4e91
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 13:24:18 +0900

    Added comments

--- src/section5/section5.4a.kt
+++ src/section5/section5.4a.kt
@@ -1,13 +1,25 @@
 package section5
 
+/**
+ * Add a public val debug to the companion object,
+ * then add a log function outside of the companion object
+ * to display the value of debug.
+ * Then add the const keyword to debug.
+ */
 class MetaData {
 
     companion object {
 
     }
-
 }
 
+/**
+ * This exercise will demonstrate the effect of a public value
+ * inside a companion object.
+ * First the value will be made public, then made const and finally
+ * made private to demonstrate the effects of generating extra methods.
+ */
 fun main(args: Array<String>) {
+    // Create an instance of MetaData and then call its log function
 
 }
