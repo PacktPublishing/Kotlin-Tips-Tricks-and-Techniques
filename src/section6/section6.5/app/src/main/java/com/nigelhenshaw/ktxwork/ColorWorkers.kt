3337d4581c4b38b086b533b302bba887fc506ccf
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 20:13:56 +0900

    Added comments

--- src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/ColorWorkers.kt
+++ src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/ColorWorkers.kt
@@ -3,7 +3,21 @@ package com.nigelhenshaw.ktxwork
 import com.nigelhenshaw.ktxwork.MainActivity.Companion.KEY_GET_COLOR
 import com.nigelhenshaw.ktxwork.MainActivity.Companion.KEY_SET_COLOR
 
+/**
+ * This collection will be used by both workers
+ * which will either set the data or get the data
+ */
 private val colors by lazy {
     arrayOfNulls<String>(4)
 }
+
+/**
+ * Create a worker to populate colors
+ */
+
+
+/**
+ * Create a worker to set colors to the
+ * output data
+ */
 
--- src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
+++ src/section6/section6.5/app/src/main/java/com/nigelhenshaw/ktxwork/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+/**
+ * This exercise will demonstrate the usage of two Workers.
+ * One to set data in a background task. And one which will
+ * update the main UI thread when the data changes.
+ * The data will be used to update an adapter to be used
+ * by a ListView.
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -17,8 +23,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create an array of colors
+
+        // Pass the array to a Data object
+
+        // Create a WorkRequest with the data passed to inputData
+
+        // Queue add colors WorkRequest for execution
+
+        // Create a get colors WorkRequest
+
+        // Setup an observer which will return color collection,
+        // which will be passed to loadList for updating the ListView
+
+        // Queue get colors WorkRequest
+
     }
 
+    /**
+     * This function will be called from the getColorsWorker WorkRequest Observer
+     * which will update the adapter with colors parameter
+     */
     private fun loadList(colors: Array<String?>?) {
         colors?.let {
             val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, colors)
