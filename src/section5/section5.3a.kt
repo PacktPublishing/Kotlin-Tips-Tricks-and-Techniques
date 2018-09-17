368ea64eddb4f316b10538c9f5c6fcdb597cf2c1
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 13:07:14 +0900

    Added comments

--- src/section5/section5.3a.kt
+++ src/section5/section5.3a.kt
@@ -1,9 +1,24 @@
 package section5
 
+/**
+ * This exercise will demonstrate the overheads of capturing lambda expressions
+ * and none capturing lamda expressions.
+ */
 fun main(args: Array<String>) {
+    // Create a value called expression. This will be used to create
+    // a capturing expression.
+
+    // Call the functionArgument method, first with the capturing expression
+    // and then with a none capturing expression.
 
 }
 
+/**
+ * Higher order function which will first have
+ * a capturing expression passed to it.
+ * After that a none-caputering expression will
+ * be passed.
+ */
 fun functionArgument(task: () -> Unit) {
     task()
 }
