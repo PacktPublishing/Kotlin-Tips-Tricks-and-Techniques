8f8c648884aa4c215d184922da9fbdfd4a2c4c74
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 13:15:24 +0900

    Added comments

--- src/section5/section5.3b.kt
+++ src/section5/section5.3b.kt
@@ -1,9 +1,19 @@
 package section5
 
+/**
+ * This exercise will demonstrate the effects of boxing and unboxing.
+ */
 fun main(args: Array<String>) {
-
+    // Call functionArgumentLamda with a lambda argument
+    // that matches the function type
 }
 
+/**
+ * First this function will be called in its current form
+ * to demostrate boxing and unboxing
+ * Then the function will be inlined to show the removal
+ * of boxing and unboxing.
+ */
 fun functionArgumentLambda(task: (Int, Int) -> Int) {
     println(task(2, 3))
 }
