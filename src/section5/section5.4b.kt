967cba635828408a4a3a0da542d9599730097cec
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 13:31:23 +0900

    Added comments

--- src/section5/section5.4b.kt
+++ src/section5/section5.4b.kt
@@ -1,5 +1,10 @@
 package section5
 
+/**
+ * Add public var javaData to the companion object.
+ * After that the @JvmField annotation be added to
+ * javaData
+ */
 class MetaJava {
 
     companion object {
@@ -7,6 +12,13 @@ class MetaJava {
 
     }
 }
+
+/**
+ * This exercise will demonstrate the additional methods
+ * created with a public value inside a companion object.
+ * Then the @JvmField will be applied to the value and
+ * the results observed in the equivalent Java code.
+ */
 fun main(args: Array<String>) {
     println(MetaJava.javaData)
 }
