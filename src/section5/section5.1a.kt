a431181583f95c6920b60384ed6b9cea621b2daa
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 11:36:26 +0900

    Added comments

--- src/section5/section5.1a.kt
+++ src/section5/section5.1a.kt
@@ -2,9 +2,29 @@ package section5
 
 import kotlin.system.measureTimeMillis
 
+/**
+ * The purpose of this exercise is to compare two kotlin loop functions
+ * to see how long they take to complete an operation.
+ * And then use the Kotlin Bytecode tool to help identify
+ * any potential bottlenecks.
+ */
 fun main(args: Array<String>) {
 
     val loopTotal = 10_000_000
+    // Measure calledForLoop() duration time using measureTimeMillis{}
+
+    // Measure calledForEach() duration time using measureTimeMillis{}
 
 }
 
+
+/**
+ * Create a function calledForLoop() which will execute an operation
+ * in a standard loop
+ */
+
+
+/**
+ * Create a function calledForEach() which will execute the forEach
+ * extension method on a collection
+ */
