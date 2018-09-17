2bab06ab4d04c618d7b3fd5ea4f9b5c4b7a595f1
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 12:44:48 +0900

    Added comments

--- src/section5/section5.2a.kt
+++ src/section5/section5.2a.kt
@@ -1,13 +1,21 @@
 package section5
 
+/**
+ * Class to be used as the type for the var backgroundThread
+ */
 class BackGroundThread : Thread() {
     override fun run() {
         println(Thread.currentThread())
     }
 }
 
+// This value will get converted to a lateinit value
 var backgroundThread: BackGroundThread? = null
 
+/**
+ * This exercise will demonstrate the difference in implementation
+ * between a nullable value and lateinit value using the var backgroundThread
+ */
 fun main(args: Array<String>) {
 
     createBackgroundThread()
