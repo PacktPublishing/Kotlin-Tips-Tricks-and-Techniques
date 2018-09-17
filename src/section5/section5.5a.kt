4e953b98bfb87d77332552c9ff0b24c9536deaf5
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 13:43:23 +0900

    Added comments

--- src/section5/section5.5a.kt
+++ src/section5/section5.5a.kt
@@ -1,5 +1,8 @@
 package section5
 
+/**
+ * Class to be used to creating a collection
+ */
 class MediaMetaSequence(
         val id: Int = 0,
         val title: String,
@@ -7,8 +10,16 @@ class MediaMetaSequence(
         val album: String
 )
 
+/**
+ * This exercise will demonstrate the impact of calling
+ * multiple chained operations on a collection.
+ * Then a new function will be created, displayTitleSequences
+ * which will use sequences for improved performance.
+ */
 fun main(args: Array<String>) {
 
+    // The collection which will be used for comparing
+    // chained operations against sequences
     val musicSorce = listOf(
             MediaMetaSequence(
                     100,
@@ -36,10 +47,23 @@ fun main(args: Array<String>) {
             )
     )
 
+    // In the second part of the exercise replace displayAlbum titles
+    // the newly created function displayAlbumTitles
     val albumTitles = displayAlbumTitles(musicSorce, "Come Fly with me")
     println("title: $albumTitles")
 }
 
+/**
+ * Create a function called displayTitleSequences.
+ * This will be used to filter & map this list
+ */
+
+
+/**
+ * This function will be used at the start of the exercise to
+ * demonstrate the extra lists created when chaining collection
+ * operations
+ */
 fun displayAlbumTitles(source: List<MediaMetaSequence>, album: String): List<String> {
     return source.filter {
         it.album == album
