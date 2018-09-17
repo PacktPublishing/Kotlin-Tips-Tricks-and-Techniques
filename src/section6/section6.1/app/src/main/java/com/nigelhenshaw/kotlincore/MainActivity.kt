343793ec919415db95ff992bd04651c79a96136a
Nigel Henshaw <nigel@mobapptuts.com>
2018-09-17 14:29:48 +0900

    Added comments

--- src/section6/section6.1/app/src/main/java/com/nigelhenshaw/kotlincore/MainActivity.kt
+++ src/section6/section6.1/app/src/main/java/com/nigelhenshaw/kotlincore/MainActivity.kt
@@ -3,8 +3,17 @@ package com.nigelhenshaw.kotlincore
 import android.app.Activity
 import android.os.Bundle
 
+/**
+ * This exercise will demonstrate the benefit of the KTX core module.
+ * The Toast method will be used as an example where its usage
+ * will be compared against the KTX core toast() method.
+ */
 class MainActivity : Activity() {
 
+    /**
+     * First implement the standard Android Toast() call.
+     * Then replace that with the KTX Core toast() method.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
